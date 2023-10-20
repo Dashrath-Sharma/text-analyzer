fddb5b221046c0656a21727946f257b75f0a1781
Dashrath-Sharma <dashrath9958@gmail.com>
2023-10-20 17:36:11 +0530

    two functions left to write

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -5,4 +5,36 @@
 // findLongestWord
 // calculateReadingTime
 
-import { pronouns } from '../data';
+import { pronounsObject } from "../data";
+
+export function countWords(string) {
+  return string.match(/\b[^\s\n]+\b/gm).length;
+}
+
+export function countCharacters(string) {
+  return string.split('').length;
+}
+
+export function countSentences(string) {
+  return string.split(/\./gm).length;
+}
+
+export function countParagraphs(string) {}
+
+export function countPronouns(string) {
+  return string
+    .match(/[a-z]+/gim)
+    .reduce((s, x) => (s += pronounsObject[x.toLowerCase()] ? 1 : 0), 0);
+}
+
+export function getReadingTime(string) {}
+
+export function getLongestWord(string) {
+    string = string.split(/\s/gmi)
+    const logestWord = string.reduce((s,x) => {
+        let len = x.length;
+        let slen = s?.length;
+       return slen<len ? x : s;
+      },'');
+      return logestWord;
+}
